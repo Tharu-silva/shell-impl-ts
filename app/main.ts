e2cfa029aa46ccee0c9dccec8ef8afb4e45c9944
Tharu Olupathage <tharu.olupathage@cea.com.au>
2025-12-16 14:56:40 +1100

    implement type

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,14 @@
 import { createInterface } from "readline";
 
+const BUILT_INS: String[] = ['echo', 'exit', 'type'];
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// TODO: Uncomment the code below to pass the first stage
-
-
-function prompt_shell(): Promise<String> { 
+function prompt_shell(): Promise<String> 
+{ 
   return new Promise((resolve) => {
     rl.question("$ ", (answer) => {
       resolve(answer);
@@ -16,18 +16,38 @@ function prompt_shell(): Promise<String> {
   })
 }
 
-while (true) {
+function handle_type(cmd: String) 
+{
+  if (BUILT_INS.includes(cmd))
+  {
+    rl.write(`${cmd} is a shell builtin\n`);
+  } else 
+  {
+    rl.write(`${cmd}: not found\n`);
+  }
+}
+
+while (true) 
+{
   let inp: String = await prompt_shell();
-  let cmd: String; let args: String[];
+  let cmd: String; 
+  let args: String[];
 
   [cmd, ...args] = inp.split(" ");
 
-  if (cmd === "exit") {
+  if (cmd === "exit") 
+  {
     rl.close();
     break; 
-  } else if (cmd === "echo") {
+  } else if (cmd === "echo") 
+  {
     rl.write(`${args.join(' ')}\n`);
-  } else {
+  } else if (cmd == "type") 
+  {
+    handle_type(args[0]);
+  }
+  else 
+  {
     rl.write(`${cmd}: command not found\n`);
   }
 }
